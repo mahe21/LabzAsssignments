cf3bbc4ad6ef247f7663923e9d29ee0f02f6c7fc
Mahesh <Maheshk2104@gmail.com>
2023-03-22 09:26:40 +0530

    Emp daily wage

---
+++ GetWage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class GetWage
+    {
+        public static void DailyWage()
+        {
+            int WAGE_PER_HOUR = 20;
+            int HOUR_PER_DAY = 8;
+            int EmpWage = (WAGE_PER_HOUR * HOUR_PER_DAY);
+            Console.WriteLine("Employee wage per day is "+EmpWage);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetWage.DailyWage();
+        }
+    }
+}
