8d33c219fd3edcf838e7dc07419cf809eacb4f8a
Mahesh <Maheshk2104@gmail.com>
2023-04-02 14:14:41 +0530

    Swap

--- source/repos/BasicCorePrograms/BasicCorePrograms/Program.cs
+++ source/repos/BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -9,10 +9,13 @@
             /*int num = int.Parse(Console.ReadLine());
             Powerof2.PowerOfTwo(num);      //Call this method to execute Powerof2.cs */
 
-          
-            int Nthnumber = int.Parse(Console.ReadLine());
-            HarmonicNum.HarNumber(Nthnumber);
 
+            /* int Nthnumber = int.Parse(Console.ReadLine());
+            HarmonicNum.HarNumber(Nthnumber);    // call this method to execute nth harmonic number */
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            SwapTwoNumbers.Swap(a, b);
         }
     }
 }
---
+++ source/repos/BasicCorePrograms/BasicCorePrograms/SwapTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class SwapTwoNumbers
+    {
+        public static void Swap(int a, int b)
+        {
+            
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
