cf853388c61ce4527817cdb3f41c2cf6ff5b3719
Mahesh <Maheshk2104@gmail.com>
2023-04-02 13:58:02 +0530

    Harmonic

---
+++ source/repos/BasicCorePrograms/BasicCorePrograms/HarmonicNum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class HarmonicNum
+    {
+        public static void HarNumber(int Nthnumber)
+        {
+            float Num = 0;
+            for (int i = 1; i <= Nthnumber; i++)
+            {
+                Num += (float)1 / i;
+
+            }
+            Console.WriteLine(Num);
+        }
+    }
+}
--- source/repos/BasicCorePrograms/BasicCorePrograms/Program.cs
+++ source/repos/BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int num = int.Parse(Console.ReadLine());
-            Powerof2.PowerOfTwo(num);
+            /*LeapYr.LeapYear();     //call this method to execute LeapYr.cs */
+
+            /*int num = int.Parse(Console.ReadLine());
+            Powerof2.PowerOfTwo(num);      //Call this method to execute Powerof2.cs */
+
+          
+            int Nthnumber = int.Parse(Console.ReadLine());
+            HarmonicNum.HarNumber(Nthnumber);
+
         }
     }
 }
