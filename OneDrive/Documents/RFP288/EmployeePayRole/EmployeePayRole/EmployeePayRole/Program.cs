f0cb68955eaeff9eb16a43a4eef9cab4d99aa876
Mahesh <Maheshk2104@gmail.com>
2023-06-18 12:14:48 +0530

    [Mahesh]Add: Uc1

---
+++ OneDrive/Documents/RFP288/EmployeePayRole/EmployeePayRole/EmployeePayRole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeePayRole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeePayRole employeePayRole = new EmployeePayRole();
+            employeePayRole.EmployeeAttendence();
+        }
+    }
+}
